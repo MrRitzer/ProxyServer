30c445c6d7332f6b1ae66792a6d7150d9e5590e5
safifi <salma.afifi@ndsu.edu>
2022-09-01 12:36:56 -0500

    Hello World!

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         new ProxyServer().startServer(Integer.parseInt(args[0]));
+        System.out.println("Hello World!");
     }
 }
