380c7c66629ad3c8e11ee90b61985f26aa7b4369
safifi <salma.afifi@ndsu.edu>
2022-09-08 14:41:00 -0500

    Logging

--- src/ProxyServer.java
+++ src/ProxyServer.java
@@ -1,26 +1,17 @@
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.net.InetAddress;
 import java.net.ServerSocket;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.sql.Timestamp;
-import java.util.Date;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
-
 public class ProxyServer {
-	//cache is a Map: the key is the URL and the value is the file name of the file that stores the cached content
+	// cache is a Map: the key is the URL and the value is the file name of the file
+	// that stores the cached content
 	Map<String, String> cache;
-	
+
 	ServerSocket proxySocket;
 
 	Boolean running;
@@ -29,7 +20,7 @@ public class ProxyServer {
 
 	void startServer(int proxyPort) {
 		cache = new ConcurrentHashMap<>();
-		// create the directory to store cached files. 
+		// create the directory to store cached files.
 		File cacheDir = new File("cached");
 		if (!cacheDir.exists() || (cacheDir.exists() && !cacheDir.isDirectory())) {
 			cacheDir.mkdirs();
@@ -44,10 +35,10 @@ public class ProxyServer {
 		System.out.println("Listening for connection: ");
 		while (true) {
 			try {
-                new RequestHandler( proxySocket.accept(), this );
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+				new RequestHandler(proxySocket.accept(), this);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
@@ -61,34 +52,34 @@ public class ProxyServer {
 
 	public synchronized void writeLog(String info) {
 
-        Logger logger = Logger.getLogger("ProxyLog");
+		//Logger logger = Logger.getLogger("ProxyLog");
+		Logger logger = Logger.getLogger(ProxyServer.class.getName());
 
-        FileHandler proxyFileHandler;
+		FileHandler proxyFileHandler;
 
-        try {
-            proxyFileHandler = new FileHandler("C:/Users/salma/OneDrive/Desktop/proxy.txt");
-            logger.addHandler(proxyFileHandler);
+		try {
+			proxyFileHandler = new FileHandler("C:/Users/salma/OneDrive/Desktop/proxy.txt");
+			logger.addHandler(proxyFileHandler);
 
-            SimpleFormatter formatter = new SimpleFormatter();
-            proxyFileHandler.setFormatter(formatter);
+			SimpleFormatter formatter = new SimpleFormatter();
+			proxyFileHandler.setFormatter(formatter);
 
-         logger.info("Hello");
-        } catch (SecurityException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+			logger.info("Hello");
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-        logger.info("Hi Everyone");
+		logger.info("Hi Everyone");
 
-    }
-
-
-		
-			/**
-			 * To do
-			 * write string (info) to the log file, and add the current time stamp 
-			 * e.g. String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
-			 *
-			*/
 	}
+
+	/**
+	 * To do
+	 * write string (info) to the log file, and add the current time stamp
+	 * e.g. String timeStamp = new
+	 * SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+	 *
+	 */
+}
