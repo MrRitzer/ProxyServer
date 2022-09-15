7d4f22c35b04a01a19608847f0d6936dce99eab9
Noah Ritter <nmritter02@gmail.com>
2022-09-15 13:43:18 -0500

    Final changes

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        new ProxyServer().startServer(4040);
+        new ProxyServer().startServer(Integer.parseInt(args[0]));
     }
 }
